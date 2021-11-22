66d20a7d5d8144ea16b59381bdc896974682025e
unknown <a01026848@itesm.mx>
2021-11-22 15:03:31 -0600

    User input and response with action

--- actions/actions.py
+++ actions/actions.py
@@ -8,16 +8,22 @@
 # This is a simple example for a custom action which utters "Hello World!"
 import datetime as dt
 from typing import Any, Text, Dict, List
+from transformers import pipeline
+classifier = pipeline("zero-shot-classification")
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 class ActionHelloWorld(Action):
     
-    chiefSigns = open("chiefSigns.txt", "r")
-    labels = []
-    for e in chiefSigns:
-        labels.append(e)
+    
+    
+    def obtain_top_labels(userI, limit, labels):
+        ans = classifier(userI, labels)
+        ans_top = {}
+        for i in range(limit):
+            ans_top[ans['labels'][i]] = ans['scores'][i]
+        print(ans_top)
 
     def name(self) -> Text:
         return "action_return_prob"
@@ -25,8 +31,13 @@ class ActionHelloWorld(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+        chiefSigns = open("chiefSigns.txt", "r")
+        candlabels = chiefSigns.readlines()
+        ans = classifier(tracker.latest_message['text'], candlabels)
+        prob = {}
+        for i in range(10):
+            prob[ans['labels'][i]] = ans['scores'][i]
+        dispatcher.utter_message(text=f"{prob}")
 
         return []
 
